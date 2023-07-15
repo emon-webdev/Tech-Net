5b5595e4711ac29c82888bff8a5aab47f5698317
emon-webdev <emon.webdev@gmail.com>
2023-07-15 14:13:47 +0600

    rtl query add

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -3,11 +3,13 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 interface ICart {
     products: IProduct[]
+    total: number
 }
 
 
 const initialState: ICart = {
     products: [],
+    total: 0
 }
 
 
@@ -16,10 +18,34 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action: PayloadAction<IProduct>) => {
-            state.products.push(action.payload);
-        }
+            const existingProduct = state.products.find(product => product._id === action.payload._id)
+            if (existingProduct) {
+                existingProduct.quantity = existingProduct.quantity! + 1;
+            } else {
+                state.products.push({ ...action.payload, quantity: 1 });
+            }
+
+            state.total += action.payload.price;
+
+        },
+        removeOne: (state, action: PayloadAction<IProduct>) => {
+            const existingProduct = state.products.find(product => product._id === action.payload._id)
+            if (existingProduct && existingProduct.quantity! > 1) {
+                existingProduct.quantity = existingProduct.quantity! - 1;
+            } else {
+                state.products = state.products.filter(product => product._id !== action.payload._id)
+            }
+
+            state.total -= action.payload.price;
+
+        },
+        removeFromCart: (state, action: PayloadAction<IProduct>) => {
+            state.products = state.products.filter(product => product._id !== action.payload._id)
+            state.total -= action.payload.price * action.payload.quantity!;
+        },
+
     }
 })
 
-export const { addToCart } = cartSlice.actions
+export const { addToCart, removeFromCart, removeOne } = cartSlice.actions
 export default cartSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { api } from './api/apiSlice';
 import cartReducer from './features/cart/cartSlice';
+import productSlice from './features/products/productSlice';
 
 export const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        product: productSlice,
+        [api.reducerPath]: api.reducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
